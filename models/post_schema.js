2aad10332b4dc15296947df3a373ecbf8c79a5d9
unknown <phsaem98@gmail.com>
2022-01-28 13:09:01 +0900

    DB insert
    Write to DB
    Banner link added

--- models/post_schema.js
+++ models/post_schema.js
@@ -1,36 +1,41 @@
-const models = require("../models");
+const models = require('../models');
 module.exports = (sequelize, DataTypes) => {
+	const Post = sequelize.define(
+		'Post',
+		{
+			post_ID: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			post_title: {
+				type: DataTypes.STRING(10),
+				allowNull: false,
+			},
+			post_date: {
+				type: DataTypes.DATE,
+				allowNull: false,
+				defaultValue: sequelize.literal('now()'),
+			},
+			post_content: {
+				type: DataTypes.TEXT,
+			},
+		},
+		{
+			charset: 'utf8', // 한국어 설정
+			collate: 'utf8_general_ci', // 한국어 설정
+			tableName: 'post_table', // 테이블 이름
+			timestamps: false,
+		}
+	);
+	Post.associate = function (models) {
+		models.Post.belongsTo(models.Member, {
+			foreignKey: 'post_writer',
+		});
+		models.Post.belongsTo(models.Board, {
+			foreignKey: 'post_category',
+		});
+	};
 
-    const Post = sequelize.define("Post", {
-        post_ID: {
-            type: DataTypes.INTEGER,
-            primaryKey: true
-        },
-        post_title: {
-            type: DataTypes.STRING(10),
-            allowNull: false
-        },
-        post_date: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        post_content: {
-            type: DataTypes.TEXT
-        }
-    }, {
-        charset: "utf8", // 한국어 설정
-        collate: "utf8_general_ci", // 한국어 설정
-        tableName: "post_table", // 테이블 이름
-        timestamps: false
-    });
-    Post.associate = function(models) {
-        models.Post.belongsTo(models.Member, {
-            foreignKey: "post_writer"
-        });
-        models.Post.belongsTo(models.Board, {
-            foreignKey: "post_category"
-        });
-    };
-
-    return Post;
+	return Post;
 };
